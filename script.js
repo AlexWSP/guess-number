1f731d40c9ed1d3de555fc49e8767915d7b6a532
Alex <spwang1996@gmail.com>
2023-02-16 23:36:43 +0800

    show play again btn after correct guess

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
 
+    document.querySelector(".again").style.visibility = "visible";
+
     if (score > highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
